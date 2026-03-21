5e5bab23be0e449c217f4e7556e14d74654c9780
rudra-the-dev <kirsndubey@gmail.com>
2026-03-21 22:09:27 +0530

    Create FlowScript service.java

---
+++ FlowScript service.java
@@ -0,0 +1,40 @@
+package app.flowscript;
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+import android.graphics.Rect;
+
+public class FlowScriptService extends AccessibilityService {
+
+    private static FlowScriptService instance;
+
+    public static FlowScriptService getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onServiceConnected() {
+        instance = this;
+        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
+        info.flags = AccessibilityServiceInfo.FLAG_REPORT_VIEW_IDS
+                   | AccessibilityServiceInfo.FLAG_RETRIEVE_INTERACTIVE_WINDOWS;
+        info.notificationTimeout = 100;
+        setServiceInfo(info);
+    }
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {}
+
+    @Override
+    public void onInterrupt() {}
+
+    @Override
+    public void onDestroy() {
+        instance = null;
+        super.onDestroy();
+    }
+}
